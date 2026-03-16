0b6645627e48cf710bb60d452fce7c2d8e1405ab
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-03-16 15:20:02 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const image = document.querySelectorAll('.list-item');
+const mainImage = document.querySelector('.gallery__large-img');
+
+image.forEach((img) => {
+  img.addEventListener('click', () => {
+    event.preventDefault();
+
+    let newSrc;
+
+    if (event.target.tagName === 'IMG') {
+      newSrc = event.target.parentNode.href;
+    } else if (event.target.tagName === 'A') {
+      newSrc = event.target.href;
+    }
+
+    mainImage.src = newSrc;
+  });
+});
